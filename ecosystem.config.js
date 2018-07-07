a33f85390ad795e13ea2abf1f22b556223fee1e1
Dan Mello <jdanmello@gmail.com>
2018-07-07 14:01:19 -0400

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name : 'mellocloud.com',
+      script : './index.js',
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'deploy',
+      host: '10.0.0.201', // local ip or public ip if im not connect to local connection
+      ref: 'origin/master',
+      repo: 'https://github.com/DanMello/mellocloud.com.git',
+      path: '/home/deploy/mello.com',
+      'post-deploy' : 'nvm install && npm install && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only mellocloud.com'
+    }
+  }
+};
