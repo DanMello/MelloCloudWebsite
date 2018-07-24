66261d301cbb094f6d8f01b9b8d606cfe5541c66
Dan Mello <jdanmello@gmail.com>
2018-07-24 19:47:33 -0400

    setting react app on server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,8 +15,8 @@ module.exports = {
       host: '10.0.0.201', // local ip or public ip if im not connect to local connection
       ref: 'origin/master',
       repo: 'https://github.com/DanMello/mellocloud.com.git',
-      path: '/home/deploy/mello.com',
-      'post-deploy' : 'nvm install && npm install && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only mellocloud.com'
+      path: '/home/deploy/mellocloud/mellocloud.com',
+      'post-deploy' : 'nvm install && npm install && npm run build && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only mellocloud.com'
     }
   }
 };
