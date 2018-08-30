e3687b1d95fc0aa662f931e23573e83471325fed
Dan Mello <mello@Dans-MacBook-Pro.local>
2018-08-30 03:10:31 -0400

    Creating routes to verify user and return their profile

--- routes.js
+++ routes.js
@@ -1,15 +1,17 @@
 
 exports = module.exports = function(app) {
 
-  
-
   //Load react js bundle
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 
-  //api routes
+  //api post routes
   app.post('/account/login', require('./src/api/userVerification/login').login)
   app.post('/account/signup', require('./src/api/userVerification/signup').signup)
   app.post('/account/signup/emailCheck', require('./src/api/userVerification/emailcheck').checkEmail)
+  
+  //api get routes
+  app.get('/account/profile/:token', require('./src/api/userVerification/profile').getProfile)
+
 }
