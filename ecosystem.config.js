c36fb6c40859760fceeefb5c3686f56a6e0d15c9
Dan Mello <jdanmello@gmail.com>
2018-09-17 03:16:40 -0400

    fixed type in config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name : 'MelloCloudWebite',
+      name : 'MelloCloudWebsite',
       script : './index.js',
     }
   ],
@@ -10,9 +10,9 @@ module.exports = {
       user: 'deploy',
       host: '10.0.0.201', // local ip or public ip if im not connect to local connection
       ref: 'origin/master',
-      repo: 'https://github.com/DanMello/mellocloud.com.git',
+      repo: 'https://github.com/DanMello/MelloCloudWebsite.git',
       path: '/home/deploy/MelloCloud/MelloCloudWebsite',
-      'post-deploy' : 'nvm install && npm install && npm run build && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only MelloCloudWebite'
+      'post-deploy' : 'nvm install && npm install && npm run build && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only MelloCloudWebsite'
     }
   }
 }
