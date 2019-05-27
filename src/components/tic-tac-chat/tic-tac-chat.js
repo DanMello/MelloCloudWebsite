23124bc46e1180098a769f3b40259e73e96d3b70
Dan Mello <jdanmello@gmail.com>
2019-05-27 17:25:09 -0400

    Added title to tic-tac-chat app

--- src/components/tic-tac-chat/tic-tac-chat.js
+++ src/components/tic-tac-chat/tic-tac-chat.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Game } from 'tic-tac-chat';
-import { FaCloud } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaCloud } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import 'tic-tac-chat/dist/main.css';
 import './tictac.css';
 
@@ -9,6 +10,9 @@ export default function TicTacChat({config}) {
   let { isMobile } = config; 
   return (
     <div className={isMobile ? 'tic-tac-mainContainer-mobile' : 'tic-tac-mainContainer'}>
+      <Helmet>
+        <title>Tic-Tac-Chat : Chat and play tic tac toe online with friends.</title>
+      </Helmet>
       <div className={isMobile ? 'tic-tac-subContainer-mobile' : 'tic-tac-subContainer'}>
         <Game config={config} />
         <div className='contact-logo-container tic-tac-marginTop'>
